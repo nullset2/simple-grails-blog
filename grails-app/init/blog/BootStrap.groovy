158bc28754ab73826e30cd656b082a2153bb474c
alfredo <jalfredo.gallegos@gmail.com>
2017-07-25 03:00:06 -0500

    fixes for prod

--- grails-app/init/blog/BootStrap.groovy
+++ grails-app/init/blog/BootStrap.groovy
@@ -4,7 +4,8 @@ class BootStrap {
 
     def init = { servletContext ->
         ["Projects","TV","Games","Movies","Internet","Tech","Programming","Music","Random"].each {
-        	new Tag(name: it).save()
+        	def tag = new Tag(name: it)
+        	tag.save()
         }
     }
     def destroy = {
